323fefdfc802b4a6ba977714d9d705d327c9a3ec
Avinash-R-29 <7338avinash@gmail.com>
2022-12-15 18:22:59 +0530

    sadfg

---
+++ 10newLabActivity/src/main/java/com/NewLabActivity/newLabActivity/App.java
@@ -0,0 +1,13 @@
+package com.NewLabActivity.newLabActivity;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
